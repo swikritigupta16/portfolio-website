8f5a56ad840d826603010e19bcaa4434d41d1eb5
unknown <swikritigupta16@gmail.com>
2025-06-16 15:34:32 +0530

    Updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,19 +9,18 @@ export default function Navbar() {
   ];
 
   return (
-    <header data-aos="fade-up" className="absolute top-0 flex justify-center items-center body-font z-20">
+    <header data-aos="fade-down" className="fixed top-0 left-0 w-full bg-blue-600 shadow-md z-50">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 mr-11 font-bold text-white text-3xl">Portfolio </span>
+        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <span className="ml-3 mr-11 font-bold text-2xl">Portfolio</span>
         </a>
-        <nav className="md:ml-auto md:mr-auto font-medium flex flex-wrap items-center text-base text-gray-300 justify-center">
+        <nav className="md:ml-auto md:mr-auto font-medium flex flex-wrap items-center text-base text-white justify-center">
           {NavbarLinks.map((e) => (
-            <a key={e.id} href={e.link} className="mr-7 hover:text-white">
+            <a key={e.id} href={e.link} className="mr-7 hover:text-yellow-300">
               {e.name}
             </a>
           ))}
         </nav>
-        
       </div>
     </header>
   );
